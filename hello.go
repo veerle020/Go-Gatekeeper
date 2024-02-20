1ed07a9b398cd7b236bb86200cbe3a3e694a2380
VeerlevanWees <veerlevanrijen@gmail.com>
2024-02-20 14:30:07 +0100

    V2

--- hello.go
+++ hello.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	currentTime := time.Now()
+	hour := currentTime.Hour()
+	var textLine string
+
+	if hour > 7 && hour < 12 {
+		textLine = "Goedemorgen!"
+	} else if hour > 12 && hour < 18 {
+		textLine = "Goedemiddag!"
+	} else if hour > 18 && hour < 23 {
+		textLine = "Goedeavond!"
+	} else {
+		textLine = "Sorry, de parkeerplaats is ’s nachts gesloten"
+	}
+
+	fmt.Println(textLine)
+
+	if hour > 7 && hour < 23 {
+		fmt.Println("Welkom bij Fonteyn Vakantieparken")
+	}
+
 }
